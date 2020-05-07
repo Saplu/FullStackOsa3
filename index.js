182868ed4d439500b6508c1c7378a70b52f5dcc0
Halti <huhtala.h@hotmail.fi>
2020-05-07 14:53:17 +0300

    herokucommit2

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ let persons = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world!<h1>')
-})
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello world!<h1>')
+// })
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
