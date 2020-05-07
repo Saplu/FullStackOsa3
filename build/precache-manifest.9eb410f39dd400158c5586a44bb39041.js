757cf6dcc2692efec37514a190b68700f7cdf276
Halti <huhtala.h@hotmail.fi>
2020-05-07 15:01:44 +0300

    herokucommit3

---
+++ build/precache-manifest.9eb410f39dd400158c5586a44bb39041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84fe48e05b510766c2da98a74a858af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbb284da86f6af88eb92",
+    "url": "/static/css/main.efa2cfb7.chunk.css"
+  },
+  {
+    "revision": "c574ff2531d61c703e37",
+    "url": "/static/js/2.16e154f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e154f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb284da86f6af88eb92",
+    "url": "/static/js/main.b5a4090d.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
